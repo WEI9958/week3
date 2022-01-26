7312e490cd0e69123bc9a79335ea65a14afbb467
WEI9958 <chenwill@sheridancollege.ca>
2022-01-26 12:30:24 -0500

    version 1

---
+++ src/gitoperations/GitOperations.java
@@ -0,0 +1,17 @@
+package gitoperations;
+
+/**
+ *This class +++Insert Description Here+++
+ *
+ * @author Paul Bonenfant
+ */
+public class GitOperations{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("to learn github operations");
+    }
+
+}
